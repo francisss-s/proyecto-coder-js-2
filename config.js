9379c5f68feb9ca1cbc016965b9b48a78cc3aa79
francisss-s <francisss-s@users.noreply.github.com>
2024-06-21 00:46:05 +0000

    deploy: ac8c8a03cef886ef9bc2f11f7ae89c189c52f025

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
+  apiKey: 'AIzaSyBc1Yc-lxevrRg7LM6rCLGY_lKEejQFmvk',
+  authDomain: 'listapeliculas-294af.firebaseapp.com',
   databaseURL: 'https://listapeliculas-294af-default-rtdb.firebaseio.com',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  projectId: 'listapeliculas-294af',
+  storageBucket: 'listapeliculas-294af.appspot.com',
+  messagingSenderId: '146290876971',
+  appId: '1:146290876971:web:da4e22c8ef14b06b01806e',
+  measurementId: 'G-SR7SD1M1NJ'
 };
 
 export const rapidApiConfig = {
-  apiKey: '',
-  apiHost: ''
+  apiKey: '57f9500b78msh7da6ebd5c5422a9p1b07adjsn0fdf9dff2727',
+  apiHost: 'imdb8.p.rapidapi.com'
 };
