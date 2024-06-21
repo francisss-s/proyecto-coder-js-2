16aa5cad30e5aa660095c215fba74666c139d053
francisco.fuentes <francisco.fuentes@west-ingenieria.cl>
2024-06-20 20:50:38 -0400

    lo intente

---
+++ config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  FIREBASE_API_KEY: 'AIzaSyBc1Yc-lxevrRg7LM6rCLGY_lKEejQFmvk',
+  FIREBASE_AUTH_DOMAIN: 'listapeliculas-294af.firebaseapp.com',
+  FIREBASE_DATABASE_URL: 'https://listapeliculas-294af-default-rtdb.firebaseio.com',
+  FIREBASE_PROJECT_ID: 'listapeliculas-294af',
+  FIREBASE_STORAGE_BUCKET: 'listapeliculas-294af.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '146290876971',
+  FIREBASE_APP_ID: '1:146290876971:web:da4e22c8ef14b06b01806e',
+  FIREBASE_MEASUREMENT_ID: 'G-SR7SD1M1NJ'
+}
+
+export const rapidApiConfig = {
+  API_RAPID: '57f9500b78msh7da6ebd5c5422a9p1b07adjsn0fdf9dff2727',
+  API_HOST: 'imdb8.p.rapidapi.com'
+}
